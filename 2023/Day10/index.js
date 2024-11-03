93d15761e5e5d89ab697ef9d73d52b2c81c69d1d
Claire <claire@outschool.com>
2024-11-03 12:53:12 -0700

    weds

--- 2023/Day10/index.js
+++ 2023/Day10/index.js
@@ -7,11 +7,7 @@ const ex1=`\
 .L-J.
 .....
 `
-// .....
-// .012.
-// .1.3.
-// .234.
-// .....
+
 const ex2=`\
 -L|F7
 7S-7|
@@ -19,6 +15,7 @@ L|7||
 -L-J|
 L|-JF
 `
+
 const ex3=`\
 ..F7.
 .FJ|.
@@ -26,69 +23,70 @@ SJ.L7
 |F--J
 LJ...
 `
-// const file = readFileSync(`./${day}/input.txt`,"utf-8").trim()  // reading files need always needs to trim that file
+
+
+const ex4 = `\
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+`
+
+
+const file = readFileSync(`./${day}/input.txt`,"utf-8").trim()  // reading files need always needs to trim that file
 
 function main(){
     console.log({day})
-    runTest( "Solve A EX1 ",solveA, ex1, null)
-    // runTest( "Solve A File",solveA, file, null) 
-    // runTest( "Solve B EX1 ",solveB, ex1, null)
+    // runTest( "Solve A EX1 ",solveA, ex1, 4)
+    // runTest( "Solve A EX2 ( messy ) ",solveA, ex2, 4)
+    // runTest( "Solve A EX3 (messy and out of bounds)",solveA, ex3, 8) 
+    // runTest( "Solve A file",solveA, file, null)  
+
+    
+    runTest( "Solve B EX1 ",solveB, ex1, 1)
+    runTest( "Solve B EX3 ",solveB, ex3, 1)
+    runTest( "Solve B EX4 ",solveB, ex4, 3)
     // runTest( "Solve B File ",solveB, file, null)
 }
 
 
 // breath first search!!! 
+function isValidMove(start, end , direction) {
+    let moves= {
+                'S' : {N: true,  S: true , E: true, W: true} ,
+                '|' : {N: true,  S: true} ,
+                '-' : {E: true,  W: true} ,
+                'L' : {N: true,  E: true} ,
+                'J' : {N: true,  W: true} ,
+                '7' : {S: true,  W: true} ,
+                'F' : {S: true,  E: true} ,
+                '.': {}
+               }
 
-function isValidMove(start, end, direction) {
-// | is a vertical pipe connecting north and south.
-// - is a horizontal pipe connecting east and west.
-// L is a 90-degree bend connecting north and east.
-// J is a 90-degree bend connecting north and west.
-// 7 is a 90-degree bend connecting south and west.
-// F is a 90-degree bend connecting south and east.
-let array1 = ['|', '-', 'L', 'J', '7', 'F'];
-    let array2 = ['N', 'E', 'S', 'W'];
-
-    // .....
-    // .F-7.
-    // .|.|.
-    // .L-J.
-    // .....
-    
-    // .F7.
-    // .LJ.
-    // .....
-const p =['||S', '||N' ,'|LE' ,'|JW', '|7W' ,'|FE',
-'--E','--W' ,'-JN', '-7S', 
-'LJE', 'L-E',
-'J|N','J7W',
-'7-W','7FW',
-'FLS','F7E']
-
-
-    // Define valid transitions for each element based on direction
-    const validTransitions = {
-      '|': { N: '|',  S: '|',  E: 'L',  W: 'J' },
-      '-': { N: 'L', S: "null", E: '-',  W: '-' },
-      'L': { N: 'null',  S: null, E: '-',  W: null },
-      'J': { N: '|',  S: null, E: null, W: '-', },
-      '7': { N: null, S: '|',  E: null, W: '-', },
-      'F': { N: null, S: '|',  E: '-',  W: null }
-    };
-    // 
-    // Check if the start and end elements can connect in the given direction
-    if (validTransitions[start] 
-        && validTransitions[start][direction] === end) {
-      return true; // Valid move
+    let opp = {
+        'N':'S',
+        'E':'W',
+        'S':'N',
+        'W':'E'
     }
-    return false; // Invalid move
-  }
-
- 
+    
+    // console.log({start,end, direction})
+    // console.log( moves[start][direction] ,  moves[end][opp[direction]] )
+    
+    if ( moves[start][direction] &&  moves[end][opp[direction]]) {
+      return true; 
+    }
+    return false; 
+}
 
 function solveA(input){
     const maze = input.trim().split("\n").map(el => el.split(""))
-    console.log({maze })
+    console.log({maze})
 
     let array1 = ['|', '-', 'L', 'J', '7', 'F'];
     let array2 = ['N', 'E', 'S', 'W'];
@@ -128,24 +126,15 @@ function solveA(input){
 
             }else {
                 visited.add(`${r},${c}`)
-                const north = current + maze[r-1][c] + "N"
-                const south = current + maze[r+1][c] + "S"
-                const east = current + maze[r][c+1] + "E"
-                const west = current + maze[r][c-1] + "W"
-                console.log({north,south,east,west})
-                // console.log("north , ",north,permutations.includes(north))
+              
+                maybeQueue(r,c,-1,0, 'N')
 
-                // console.log("s , ",north,permutations.includes(south))
+                maybeQueue(r,c,1,0, 'S')
 
+                maybeQueue(r,c,0,1, "E")
 
-                // console.log("e , ",north,permutations.includes(east))
-
-                // console.log("w , ",north,permutations.includes(west))
-
-                maybeQueue(r+1,c)
-                maybeQueue(r-1,c)
-                maybeQueue(r,c+1)
-                maybeQueue(r,c-1)
+                maybeQueue(r,c,0,-1, "W")
+                
             }
         }
         console.log({depth , queue, nextDepthQueue})
@@ -155,12 +144,17 @@ function solveA(input){
 
     }
 
-    function maybeQueue(r,c){
-        if (visited.has(`${r},${c}`)){
+    function maybeQueue(r,c, dr,dc ,move){
+        const current = maze[r][c] 
+        const target = maze[r+dr]?.[c+dc]
+        if (!target){
+            return
+        }
+        if (visited.has(`${r+dr},${c+dc}`)){
             return 
         }else {
-            if (maze[r][c] !='.' ){
-                nextDepthQueue.push({r,c})
+            if (isValidMove(current, target, move)){
+                nextDepthQueue.push({r: r+dr,c: c+dc})
             }
         }
     }
@@ -168,12 +162,13 @@ function solveA(input){
     return undefined;
 }
 
-
-
-
 function solveB(input){
     const lines = input.trim().split("\n");
 
+// replace the solution from A with a line of Xs - 
+// replace everything else with a . 
+// go through the outside ring and mark as outside - then resurcive search on that dot to finds its dfs to mark it as outside ,
+// once there are no more outside dots  count up the last of the dots and those are the inside dots 
 
    return undefined;
 }
